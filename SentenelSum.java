fdbf39074d0fdb32c876a2d74ceecd151a6135b7
JonasN28 <jonas.novak.sea@gmail.com>
2025-11-17 09:12:08 -0800

    added files
    Files are needed to code

---
+++ SentenelSum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SentenelSum {
+   public static void main(String[] args){
+   Scanner scan = new Scanner(System.in);
+   System.out.println("input an integer");
+   int number = 0;
+   int sum;
+      while(!(number == -1)){
+         number = scan.nextInt();
+      
+      }
+   }
+}
