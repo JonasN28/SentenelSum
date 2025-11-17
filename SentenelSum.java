4f23eb9f004c9c4fe46d90278bb939979f24478a
JonasN28 <jonas.novak.sea@gmail.com>
2025-11-17 09:18:34 -0800

    Made it do the thing
    I made it do the thing it's supposed to do.

--- SentenelSum.java
+++ SentenelSum.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class SentenelSum {
    public static void main(String[] args){
    Scanner scan = new Scanner(System.in);
-   System.out.println("input an integer");
    int number = 0;
-   int sum;
+   int sum = 0;
       while(!(number == -1)){
+         sum += number;
+         System.out.print("type in a number and then press enter, once you enter -1, the program will give you a sum : ");
          number = scan.nextInt();
-      
       }
+   System.out.println("The sum is : " + sum);
    }
 }
